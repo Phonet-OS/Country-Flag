a42586e8759d50afef90969c2d793df56791a7d3
Ankit Patel <36913747+soilcoded@users.noreply.github.com>
2020-02-01 20:56:08 +0530

    Initial commit

---
+++ app/src/main/java/co/phonet/countryflag/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.phonet.countryflag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
